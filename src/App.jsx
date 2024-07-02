028981be16ed3bbe39b3463dd517f87353752870
zacharybaca <zachary.baca@msn.com>
2024-07-02 14:20:26 -0500

    completed dj small function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import './App.css'
 import Square from "./components/Square";
 
 function App() {
   const [squareColors, setSquareColors] = useState(["white", "white", "white", "white"]);
+
+  // Small Time DJ Function
+  function smallTime() {
+    if (squareColors[0] === "white") {
+      setSquareColors(["black", "black", "black", "black"]);
+    }
+    else if (squareColors[0] === "black") {
+      setSquareColors(["white", "white", "white", "white"]);
+    }
+  }
 
   return (
     <div id="grid-container">
@@ -12,7 +23,7 @@ function App() {
         return <Square key={index} color={color}/>
       })}
       
-      <button>DJ Small</button>
+      <button onClick={smallTime}>DJ Small</button>
       <button>Party DJ</button>
       <button>Left Blue</button>
       <button>Right Blue</button>
